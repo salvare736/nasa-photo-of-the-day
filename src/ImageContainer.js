7adfafd3fa7d02628116b791dc5e04aed8e40544
Sergio Alvarez <salvare736@gmail.com>
2021-03-18 15:58:28 -0400

    completed some basic styling

--- src/ImageContainer.js
+++ src/ImageContainer.js
@@ -1,14 +1,35 @@
 import React from "react";
+import styled from 'styled-components';
+
+const StyledContainer = styled.div`
+  /* flex-flow: row wrap;
+  justify-content: center;
+  align-content: center; */
+`
+
+const StyledText = styled.p`
+  color: ${pr => pr.theme.secondaryColor};
+  font-weight: bold;
+  width: 25%;
+
+  &:hover {
+    color: ${pr => pr.theme.primaryColor};
+    background-color: ${pr => pr.theme.quaternaryColor};
+    transition: all 1.5s ease-in-out;
+  }
+
+  transition: all .75s ease-in-out;
+`
 
 const ImageContainer = (props) => {
   // decide what props are needed!
   const { nasaImage } = props;
 
   return (
-    <div className='data-container-wrapper'>
-      <p>This is NASA's photo of the day!</p>
+    <StyledContainer>
+      <StyledText>This is NASA's photo of the day!</StyledText>
       <img src={nasaImage} alt='Nasa'/>
-    </div>
+    </StyledContainer>
   );
 };
 
