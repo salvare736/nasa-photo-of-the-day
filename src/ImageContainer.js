ee6ce5af115a1000f97da6428112a6c7d23fdeb4
Sergio Alvarez <salvare736@gmail.com>
2021-03-18 16:31:59 -0400

    added all finishing touches on the styling

--- src/ImageContainer.js
+++ src/ImageContainer.js
@@ -2,23 +2,32 @@ import React from "react";
 import styled from 'styled-components';
 
 const StyledContainer = styled.div`
-  /* flex-flow: row wrap;
-  justify-content: center;
-  align-content: center; */
+  display: flex;
+  flex-flow: column nowrap;
+  align-items: center;
+  background-color: ${pr => pr.theme.quaternaryColor};
+  padding-bottom: 2%;
+  border: 5px dotted ${pr => pr.theme.black};
 `
 
 const StyledText = styled.p`
   color: ${pr => pr.theme.secondaryColor};
   font-weight: bold;
-  width: 25%;
+  max-width: 95%;
+  padding: 1%;
 
   &:hover {
     color: ${pr => pr.theme.primaryColor};
-    background-color: ${pr => pr.theme.quaternaryColor};
-    transition: all 1.5s ease-in-out;
+    background-color: ${pr => pr.theme.white};
+    border: 10px double ${pr => pr.theme.tertiaryColor};
+    transition: all 1s ease-in-out;
   }
 
-  transition: all .75s ease-in-out;
+  transition: all .5s ease-in-out;
+`
+
+const StyledImage = styled.img`
+  max-width: 95%;
 `
 
 const ImageContainer = (props) => {
@@ -28,7 +37,7 @@ const ImageContainer = (props) => {
   return (
     <StyledContainer>
       <StyledText>This is NASA's photo of the day!</StyledText>
-      <img src={nasaImage} alt='Nasa'/>
+      <StyledImage src={nasaImage} alt='Nasa'/>
     </StyledContainer>
   );
 };
